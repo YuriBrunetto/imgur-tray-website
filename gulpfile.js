7d73504246a179e66dd04dfbfd154cadd271f47b
Yuri Brunetto <brunettoyuri@gmail.com>
2017-06-03 03:26:36 -0300

    initial setup

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const browserSync = require('browser-sync').create()
+
+gulp.task('serve', ['sass'], () => {
+  browserSync.init({ server: './' })
+
+  gulp.watch('./scss/*.scss', ['sass'])
+  gulp.watch('./*.html').on('change', browserSync.reload)
+})
+
+gulp.task('sass', () => {
+  return gulp.src('./scss/*.scss')
+    .pipe(autoprefixer())
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('default', ['serve'])
